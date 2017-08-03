79dc6be80446bddf9516f3c31ba85332317d67a7
Slobodan Adamovic <slobodan.adamovic@customweb.com>
2017-08-03 17:54:48 +0200

    Attempt to fix deployment scripts

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "[INFO] Running before_deploy script"
-openssl aes-256-cbc -K $encrypted_85ed150a0e11_key -iv $encrypted_85ed150a0e11_iv -in codesigning.asc.enc -out codesigning.asc -d && \
+openssl aes-256-cbc -K $encrypted_85ed150a0e11_key -iv $encrypted_85ed150a0e11_iv -in codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
