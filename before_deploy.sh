59ad65ee20326d2d87711f9dc9c817fc4c2b85ae
Slobodan Adamovic <slobodan.adamovic@customweb.com>
2017-08-03 17:32:50 +0200

    Fix before_deploy script

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,4 +2,4 @@
 
 echo "[INFO] Running before_deploy script"
 openssl aes-256-cbc -K $encrypted_85ed150a0e11_key -iv $encrypted_85ed150a0e11_iv -in codesigning.asc.enc -out codesigning.asc -d && \
-gpg --fast-import signingkey.asc
+gpg --fast-import codesigning.asc
