5ccbf6b6d93fd9f9c9eda0531d13de1c84a8ad00
Slobodan Adamovic <slobodan.adamovic@customweb.com>
2017-08-04 08:51:06 +0200

    Remove old commands from before_deploy

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "[INFO] Running before_deploy script"
-openssl aes-256-cbc -K $encrypted_85ed150a0e11_key -iv $encrypted_85ed150a0e11_iv -in codesigning.asc.enc -out codesigning.asc -d
-gpg --fast-import codesigning.asc
-
 if [ -z ${TRAVIS_TAG}]; 
 then
 	echo "[INFO] This is not tagged build. The TRAVIS_TAG is not set. Skipping before_deploy step.";
