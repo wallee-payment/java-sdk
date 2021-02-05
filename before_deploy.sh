b5c78cb50cf9947d80a97a9158d8a58fde439d17
Victor Ndambakuwa <victor.ndambakuwa@wallee.com>
2021-02-05 14:47:47 +0100

    Release 2.2.17

--- before_deploy.sh
+++ before_deploy.sh
@@ -8,4 +8,5 @@ else
 	echo "[INFO] Running before_deploy script. Decrypting 'codesigning.asc.enc'..."
 	openssl aes-256-cbc -K $encrypted_85ed150a0e11_key -iv $encrypted_85ed150a0e11_iv -in codesigning.asc.enc -out codesigning.asc -d && \
 	gpg --fast-import codesigning.asc;
+    gpg --export-secret-keys >${HOME}/.gnupg/codesigning.asc;
 fi
