64e9e4f861ff843614d267ba58aa08aeeeb6e1d6
wallee-deployment-user <technical-systems@wallee.com>
2023-10-16 17:08:32 +0200

    Release 6.1.0

--- src/main/java/com/wallee/sdk/model/SubscriptionComponentConfiguration.java
+++ src/main/java/com/wallee/sdk/model/SubscriptionComponentConfiguration.java
@@ -41,8 +41,20 @@ public class SubscriptionComponentConfiguration {
   protected Long component = null;
 
   
+  @JsonProperty("id")
+  protected Long id = null;
+
+  
+  @JsonProperty("linkedSpaceId")
+  protected Long linkedSpaceId = null;
+
+  
   @JsonProperty("quantity")
   protected BigDecimal quantity = null;
+
+  
+  @JsonProperty("version")
+  protected Integer version = null;
 
   
   
@@ -57,12 +69,42 @@ public class SubscriptionComponentConfiguration {
 
   
    /**
+   * A unique identifier for the object.
+   * @return id
+  **/
+  @ApiModelProperty(value = "A unique identifier for the object.")
+  public Long getId() {
+    return id;
+  }
+
+  
+   /**
+   * The ID of the space this object belongs to.
+   * @return linkedSpaceId
+  **/
+  @ApiModelProperty(value = "The ID of the space this object belongs to.")
+  public Long getLinkedSpaceId() {
+    return linkedSpaceId;
+  }
+
+  
+   /**
    * 
    * @return quantity
   **/
   @ApiModelProperty(value = "")
   public BigDecimal getQuantity() {
     return quantity;
+  }
+
+  
+   /**
+   * The version is used for optimistic locking and incremented whenever the object is updated.
+   * @return version
+  **/
+  @ApiModelProperty(value = "The version is used for optimistic locking and incremented whenever the object is updated.")
+  public Integer getVersion() {
+    return version;
   }
 
   
@@ -77,12 +119,15 @@ public class SubscriptionComponentConfiguration {
     }
     SubscriptionComponentConfiguration subscriptionComponentConfiguration = (SubscriptionComponentConfiguration) o;
     return Objects.equals(this.component, subscriptionComponentConfiguration.component) &&
-        Objects.equals(this.quantity, subscriptionComponentConfiguration.quantity);
+        Objects.equals(this.id, subscriptionComponentConfiguration.id) &&
+        Objects.equals(this.linkedSpaceId, subscriptionComponentConfiguration.linkedSpaceId) &&
+        Objects.equals(this.quantity, subscriptionComponentConfiguration.quantity) &&
+        Objects.equals(this.version, subscriptionComponentConfiguration.version);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(component, quantity);
+    return Objects.hash(component, id, linkedSpaceId, quantity, version);
   }
 
 
@@ -92,7 +137,10 @@ public class SubscriptionComponentConfiguration {
     sb.append("class SubscriptionComponentConfiguration {\n");
     
     sb.append("    component: ").append(toIndentedString(component)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    linkedSpaceId: ").append(toIndentedString(linkedSpaceId)).append("\n");
     sb.append("    quantity: ").append(toIndentedString(quantity)).append("\n");
+    sb.append("    version: ").append(toIndentedString(version)).append("\n");
     sb.append("}");
     return sb.toString();
   }
